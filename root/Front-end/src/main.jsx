97803f881bb25f4de1a8bba090c04692d68c2e10
Louaynticha <99510252+LouayNticha@users.noreply.github.com>
2023-02-13 23:38:19 +0100

    added the docnavbar

--- root/Front-end/src/main.jsx
+++ root/Front-end/src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './CSS/index.css'
+import NAV from './components/NavBarDoctor'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <BrowserRouter><App /></BrowserRouter> 
